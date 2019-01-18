6723dc8ddedbdb0e86afe6f30f1f65a0b2048da1
HermesSJC <41092685+HermesSJC@users.noreply.github.com>
2019-01-18 17:54:41 +0800

    Add files via upload

---
+++ mainwindow.cpp
@@ -0,0 +1,369 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    //刷新串口
+    on_portNameComboBox_activated(0);
+
+    //增加波特率
+    ui->bandRateComboBox->clear();
+    ui->bandRateComboBox->addItem("9600",QSerialPort::Baud9600);
+    ui->bandRateComboBox->addItem("38400",QSerialPort::Baud38400);
+    ui->bandRateComboBox->addItem("57600",QSerialPort::Baud57600);
+    ui->bandRateComboBox->addItem("115200",QSerialPort::Baud115200);
+    ui->bandRateComboBox->setCurrentIndex(3);
+
+    //增加时间间隔
+    ui->intervalTimeComboBox->clear();
+    ui->intervalTimeComboBox->addItem("200",200);
+    ui->intervalTimeComboBox->addItem("500",500);
+    ui->intervalTimeComboBox->addItem("1000",1000);
+    ui->intervalTimeComboBox->setCurrentIndex(0);
+
+    nIndexAutorun = 0;
+    nIndexInit = 0;
+    nIndexManual = 0;
+
+    nSizeAutorun = 0;
+    nSizeInit = 0;
+    nSizeManual = 0;
+
+    isSendInit = false;
+    isSendAutorun = false;
+    isSendManual = false;
+
+    connect(&serialPort,SIGNAL(readyRead()),this,SLOT(on_serialPort_readyRead()));
+
+    //增加系统标置位
+    ui->statusBar->showMessage(GetCurrentTime() + " - " + QString::fromLocal8Bit("系统初始化完毕!"),1000);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_portNameComboBox_activated(int index)
+{
+    //清除原有内容
+    ui->portNameComboBox->clear();
+
+    //添加现有串口
+    const auto infos = QSerialPortInfo::availablePorts();
+    //如果是空的就退出
+    if(infos.isEmpty())
+    {
+        ui->portNameComboBox->addItem("None");
+        return;
+    }
+
+    //非空就添加串口
+    for(const QSerialPortInfo &info: infos)
+    {
+        ui->portNameComboBox->addItem(info.portName());
+    }
+}
+
+void MainWindow::on_bandRateComboBox_activated(int index)
+{
+
+}
+
+void MainWindow::on_connectSerialButton_clicked()
+{
+    //设置串口
+    serialPort.setPortName(ui->portNameComboBox->currentText());
+    serialPort.setBaudRate(static_cast<QSerialPort::BaudRate>(ui->bandRateComboBox->currentData().toInt()));
+    serialPort.setDataBits(QSerialPort::Data8);
+    serialPort.setParity(QSerialPort::NoParity);
+    serialPort.setStopBits(QSerialPort::OneStop);
+
+    //尝试打开串口
+    if(serialPort.open(QIODevice::ReadWrite))
+    {
+        ui->bandRateComboBox->setDisabled(true);
+        ui->portNameComboBox->setDisabled(true);
+        ui->disconnectSerialButton->setEnabled(true);
+        ui->intervalTimeComboBox->setDisabled(true);
+        ui->connectSerialButton->setDisabled(true);
+        ui->statusBar->showMessage(GetCurrentTime() + " - " + QString::fromLocal8Bit("串口打开成功!"),1000);
+    }
+    else
+    {
+        ui->statusBar->showMessage(GetCurrentTime() + " - " + QString::fromLocal8Bit("串口打开失败!"),1000);
+    }
+}
+
+void MainWindow::on_disconnectSerialButton_clicked()
+{
+    //关闭
+    serialPort.close();
+
+    //如果还打开,报错
+    if(serialPort.isOpen())
+    {
+        ui->statusBar->showMessage(GetCurrentTime() + " - " + QString::fromLocal8Bit("串口关闭失败!"),1000);
+        return;
+    }
+
+    ui->bandRateComboBox->setEnabled(true);
+    ui->portNameComboBox->setEnabled(true);
+    ui->connectSerialButton->setEnabled(true);
+    ui->disconnectSerialButton->setDisabled(true);
+    ui->intervalTimeComboBox->setEnabled(true);
+    ui->statusBar->showMessage(GetCurrentTime() + " - " + QString::fromLocal8Bit("串口打开成功!"),1000);
+
+
+}
+
+void MainWindow::on_chooseInitPathButton_clicked()
+{
+    QString filePath = QFileDialog::getOpenFileName(this,QString::fromLocal8Bit("选择文件"),currentPath.path(),tr("* txt"));
+    if(filePath.isEmpty())
+    {
+        return;
+    }
+
+    sendInitData.clear();
+    QFile file(filePath);
+    QString line;
+    if(file.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        //没到底就一直读
+        while(!file.atEnd())
+        {
+            line = QString(file.readLine());
+            int nIndexJ = line.indexOf('J');
+            if(nIndexJ != -1)
+            {
+                line.remove("\r");
+                line.remove("\n");
+                line.remove(0,nIndexJ);
+                sendInitData.append(line);
+            }
+
+        }
+        nSizeInit = sendInitData.size();
+        ui->txLineEdit->setText(sendInitData.at(0));
+        ui->initNumLabel->setText(QString::fromLocal8Bit("初始路径共%1个点").arg(nSizeInit));
+    }
+
+}
+
+void MainWindow::on_choosManualPathButton_clicked()
+{
+
+}
+
+void MainWindow::on_chooseAutorunPathButton_clicked()
+{
+    QString filePath = QFileDialog::getOpenFileName(this,QString::fromLocal8Bit("选择文件"),currentPath.path(),tr("* txt"));
+    if(filePath.isEmpty())
+    {
+        return;
+    }
+
+    sendAutorunData.clear();
+    QFile file(filePath);
+    QString line;
+    if(file.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        //没到底就一直读
+        while(!file.atEnd())
+        {
+            line = QString(file.readLine());
+            int nIndexJ = line.indexOf('J');
+            if(nIndexJ != -1)
+            {
+                line.remove("\r");
+                line.remove("\n");
+                line.remove(0,nIndexJ);
+                sendAutorunData.append(line);
+            }
+
+        }
+        nSizeAutorun = sendAutorunData.size();
+        ui->autorunNumLabel->setText(QString::fromLocal8Bit("自动路径共%1个点").arg(nSizeAutorun));
+    }
+}
+
+void MainWindow::on_intervalTimeComboBox_activated(int index)
+{
+
+}
+
+void MainWindow::on_continuousTxInitButton_toggled(bool checked)
+{
+    //判断串口是否打开
+    if(serialPort.isOpen() == false)
+    {
+        ui->statusBar->showMessage(GetCurrentTime() + " - " + QString::fromLocal8Bit("请先打开串口!"),1000);
+        ui->continuousTxInitButton->setChecked(false);
+        return;
+    }
+
+    //判断是否有足够的点
+    if(nSizeInit == 0 )
+    {
+        ui->statusBar->showMessage(GetCurrentTime() + " - " + QString::fromLocal8Bit("没有足够的点!"),1000);
+        ui->continuousTxInitButton->setChecked(false);
+        return ;
+    }
+
+    //按下
+    if(checked == true)
+    {
+        nTimerIDInit = startTimer(ui->intervalTimeComboBox->currentData().toInt());
+        isSendInit= true;
+    }
+    //松开
+    else
+    {
+        killTimer(nTimerIDInit);
+        isSendInit = false;
+    }
+}
+
+void MainWindow::on_continuousTxManualButton_toggled(bool checked)
+{
+    //判断串口是否打开
+    if(serialPort.isOpen() == false)
+    {
+        ui->statusBar->showMessage(GetCurrentTime() + " - " + QString::fromLocal8Bit("请先打开串口!"),1000);
+        return;
+    }
+}
+
+void MainWindow::on_continuousTxAutorunButton_toggled(bool checked)
+{
+    //判断串口是否打开
+    if(serialPort.isOpen() == false)
+    {
+        ui->statusBar->showMessage(GetCurrentTime() + " - " + QString::fromLocal8Bit("请先打开串口!"),1000);
+        ui->continuousTxAutorunButton->setChecked(false);
+        return;
+    }
+
+    //判断是否有足够的点
+    if(nSizeAutorun == 0 )
+    {
+        ui->statusBar->showMessage(GetCurrentTime() + " - " + QString::fromLocal8Bit("没有足够的点!"),1000);
+        ui->continuousTxAutorunButton->setChecked(false);
+        return ;
+    }
+
+    //按下
+    if(checked == true)
+    {
+        nTimerIDAutorun = startTimer(ui->intervalTimeComboBox->currentData().toInt());
+        isSendAutorun= true;
+    }
+    //松开
+    else
+    {
+        killTimer(nTimerIDAutorun);
+        isSendAutorun = false;
+    }
+
+}
+
+void MainWindow::on_clearRxTxButton_clicked()
+{
+    //ui->txLineEdit->clear();
+
+    ui->rxTextEdit->clear();
+    ui->rxTextEdit->document()->clear();
+}
+
+void MainWindow::on_singleTxButton_clicked()
+{
+    //判断串口是否打开
+    if(serialPort.isOpen() == false)
+    {
+        ui->statusBar->showMessage(GetCurrentTime() + " - " + QString::fromLocal8Bit("请先打开串口"),1000);
+        return;
+    }
+
+    serialPort.write(ui->txLineEdit->text().toLatin1().append("\r\n"));
+}
+
+void MainWindow::on_serialPort_readyRead()
+{
+    QTextCursor textCursor = ui->rxTextEdit->textCursor();
+    textCursor.movePosition(QTextCursor::End);
+    ui->rxTextEdit->setTextCursor(textCursor);
+
+    ui->rxTextEdit->insertPlainText(QString(serialPort.readAll()));
+}
+
+void MainWindow::timerEvent(QTimerEvent *event)
+{
+    //判断是不是初始
+    if(event->timerId() == nTimerIDInit && isSendInit == true)
+    {
+        //有就发送
+        if(nIndexInit < nSizeInit)
+        {
+            //发送数据
+            ui->statusBar->showMessage(GetCurrentTime() + " - " + QString::fromLocal8Bit("当前发送第%1个").arg(nIndexInit),1000);
+            serialPort.write(sendInitData.at(nIndexInit).toLatin1().append("\r\n"));
+
+            //如果是最后一个
+            if(nIndexInit == nSizeInit-1)
+            {
+                //复位清零
+                nIndexInit = 0;
+                ui->txLineEdit->clear();
+
+                emit ui->continuousTxInitButton->toggled(false);
+                ui->continuousTxInitButton->setChecked(false);
+            }
+            //不是最后一个就把下一个坐标放在文本上
+            else
+            {
+                nIndexInit ++;
+                ui->txLineEdit->setText(sendInitData.at(nIndexInit));
+            }
+        }
+        else
+        {
+            ui->statusBar->showMessage(GetCurrentTime() + " - " + QString::fromLocal8Bit("发送完毕"),1000);
+        }
+    }
+
+
+    //判断是不是自动
+    if(event->timerId() == nTimerIDAutorun && isSendAutorun == true)
+    {
+        //有就发送
+        if(nIndexAutorun < nSizeAutorun)
+        {
+            //发送数据
+            ui->statusBar->showMessage(GetCurrentTime() + " - " + QString::fromLocal8Bit("当前发送第%1个").arg(nIndexAutorun),1000);
+
+            serialPort.write(sendAutorunData.at(nIndexAutorun).toLatin1().append("\r\n"));
+
+            //如果是最后一个
+            if(nIndexAutorun == nSizeAutorun-1)
+            {
+                //复位清零
+                nIndexAutorun = 0;
+                ui->txLineEdit->clear();
+
+                emit ui->continuousTxAutorunButton->toggled(false);
+                ui->continuousTxAutorunButton->setChecked(false);
+            }
+            //不是最后一个就把下一个坐标放在文本上
+            else
+            {
+                nIndexAutorun ++;
+                ui->txLineEdit->setText(sendAutorunData.at(nIndexAutorun));
+            }
+        }
+    }
+}
---
+++ mainwindow.h
@@ -0,0 +1,90 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QString>
+#include <QTimer>
+#include <QSerialPort>
+#include <QSerialPortInfo>
+#include <QDateTime>
+#include <QFile>
+#include <QFileDialog>
+#include <QTimerEvent>
+#include <QDir>
+
+#include <QDebug>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    QSerialPort serialPort;
+
+    QDateTime dateTime;
+    inline QString GetCurrentTime(QString format);
+
+    int nIndexAutorun, nIndexInit, nIndexManual;
+    int nSizeAutorun, nSizeInit, nSizeManual;
+    bool isSendAutorun, isSendInit, isSendManual;
+    int nTimerIDAutorun, nTimerIDInit, nTimerIDManual;
+    QList<QString> sendInitData, sendAutorunData, sendManualData;
+
+    QDir currentPath;
+
+
+private slots:
+    void on_portNameComboBox_activated(int index);
+
+    void on_bandRateComboBox_activated(int index);
+
+    void on_connectSerialButton_clicked();
+
+    void on_disconnectSerialButton_clicked();
+
+    void on_chooseInitPathButton_clicked();
+
+    void on_choosManualPathButton_clicked();
+
+    void on_chooseAutorunPathButton_clicked();
+
+    void on_intervalTimeComboBox_activated(int index);
+
+    void on_continuousTxInitButton_toggled(bool checked);
+
+    void on_continuousTxManualButton_toggled(bool checked);
+
+    void on_continuousTxAutorunButton_toggled(bool checked);
+
+    void on_clearRxTxButton_clicked();
+
+    void on_singleTxButton_clicked();
+
+    void on_serialPort_readyRead();
+
+protected:
+    void timerEvent(QTimerEvent *event);
+
+private:
+    Ui::MainWindow *ui;
+};
+
+
+inline QString MainWindow::GetCurrentTime(QString format = "hh:mm:ss")
+{
+    return dateTime.currentDateTime().toString(format);
+}
+
+
+
+
+
+#endif // MAINWINDOW_H
